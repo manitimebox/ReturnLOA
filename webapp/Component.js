b9002d21c465fb2bf21b2052891ce1273e7fb70f
Mani TimeBoxLabs <S0022819506@simcoe.ca>
2023-08-09 08:58:04 +0000

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zrloaeladmin.zrloaeladmin.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
